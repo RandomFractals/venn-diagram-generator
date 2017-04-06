f4e4a89a217b98fa9a87ab43441ee7006a05428c
Taras Novak <taras.novak@randomfractals.com>
2017-04-06 10:25:16 -0500

    changed app selector to venn-diagram-app for clarity

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'venn-diagram-app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
