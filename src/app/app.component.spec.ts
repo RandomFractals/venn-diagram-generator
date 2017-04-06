db2b064116dffdedfe374de1929e40a46b862b42
Taras Novak <taras.novak@randomfractals.com>
2017-04-06 16:22:48 -0500

    app comp generated tests msgs cleanup

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -17,13 +17,13 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   }));
 
-  it(`should have as title 'Venn Diagram Generator'`, async(() => {
+  it(`should have 'Venn Diagram Generator' app title`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app.title).toEqual('Venn Diagram Generator');
   }));
 
-  it('should render title in a h1 tag', async(() => {
+  it('should render title in h1 tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
