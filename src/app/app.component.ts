22cac92038fee1ace9f654ea6c2f0bf166952a81
Taras Novak <taras.novak@randomfractals.com>
2017-04-06 11:51:03 -0500

    added starter color palette and topic stroke/fill color props for topics shape coloring later

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,14 +13,53 @@ import {Topic} from './models/topic';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-
+  // app title
   title:string = 'Venn Diagram Generator';
+
+  // Venn diagram data model
   diagram:Diagram = new Diagram('My Venn Diagram');
 
+  // starter stroke and fill color palette for topics
+  colorPalette = [
+    '#a6cee3',
+    '#1f78b4',
+    '#b2df8a',
+    '#33a02c',
+    '#fb9a99',
+    '#e31a1c',
+    '#fdbf6f',
+    '#ff7f00',
+    '#cab2d6',
+    '#6a3d9a',
+    '#ffff99',
+    '#b15928'
+  ];
+
+  /**
+   * Creates new Venn diagram designer app insntace.
+   */
   constructor() {
-    // add 3 test topics for now
-    this.diagram.topics.push(new Topic('Test 1'));
-    this.diagram.topics.push(new Topic('Test 1'));
-    this.diagram.topics.push(new Topic('Test 1'));    
+
+    // add 3 starter topics for now
+    this.diagram.topics.push(
+      new Topic('Topic 1', this.getRandomColor(), this.getRandomColor() ) // stroke, fill
+    );
+
+    this.diagram.topics.push(
+      new Topic('Topic 2', this.getRandomColor(), this.getRandomColor() )
+    );
+
+    this.diagram.topics.push(
+      new Topic('Topic 3', this.getRandomColor(), this.getRandomColor() )
+    );
+  }
+
+
+  /**
+   * Gets a random color from configured color palette
+   * for diagrom topic stroke and fill color display.
+   */
+  private getRandomColor():string {
+    return this.colorPalette[(Math.random() * 12)];
   }
 }
--- src/app/models/topic.ts
+++ src/app/models/topic.ts
@@ -2,5 +2,15 @@
  * Defines a generic topic data model type for topical maps and diagrams.
  */
 export class Topic {
-  constructor(public name:string) {}
+
+  /**
+   * Creates new topic model data instance with name, 
+   * stroke and fill color properties
+   * for a topical diagram display.
+   * 
+   * @param name Topic name.
+   * @param strokeColor Topic stroke color.
+   * @param fillColor Topic fill color.
+   */
+  constructor(public name:string, public strokeColor:string, public fillColor:string) {}
 }
