b0773441859eb5933937b065ff7211a6b55ce602
Taras Novak <taras.novak@randomfractals.com>
2017-04-06 15:45:19 -0500

    moved colorPalette, addTopic, etc. to diagram.ts view model class for easy testing later

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,56 +17,14 @@ export class AppComponent {
   // app title
   title:string = 'Venn Diagram Generator';
 
-  // Venn diagram data model
+  // Venn diagram data model instance
   diagram:Diagram = new Diagram('My Venn Diagram');
-
-  // starter stroke and fill color palette for topics
-  colorPalette = [
-    '#a6cee3',
-    '#1f78b4',
-    '#b2df8a',
-    '#33a02c',
-    '#fb9a99',
-    '#e31a1c',
-    '#fdbf6f',
-    '#ff7f00',
-    '#cab2d6',
-    '#6a3d9a',
-    '#ffff99',
-    '#b15928'
-  ];
 
   /**
    * Creates new Venn diagram designer app instance.
    */
   constructor() {
 
-    // add 3 starter topics for now
-    this.addTopic('Topic 1');
-    this.addTopic('Topic 2');
-    this.addTopic('Topic 3');
   }
 
-
-  /**
-   * Adds new topic to the diagram.
-   * 
-   * @param topicName Topic name.
-   */
-  addTopic(topicName:string) {
-    if (topicName) {
-      this.diagram.topics.push(
-        new Topic(topicName, this.getRandomColor(), this.getRandomColor() ) // stroke, fill
-      );
-    }
-  }
-
-
-  /**
-   * Gets a random color from configured color palette
-   * for diagrom topic stroke and fill color display.
-   */
-  private getRandomColor():string {
-    return this.colorPalette[(Math.random() * 12)];
-  }
 }
--- src/app/models/diagram.ts
+++ src/app/models/diagram.ts
@@ -9,10 +9,55 @@ export class Diagram {
 
   topics: Array<Topic> = new Array<Topic>();
 
+  // starter stroke and fill color palette for topics
+  colorPalette = [
+    '#a6cee3',
+    '#1f78b4',
+    '#b2df8a',
+    '#33a02c',
+    '#fb9a99',
+    '#e31a1c',
+    '#fdbf6f',
+    '#ff7f00',
+    '#cab2d6',
+    '#6a3d9a',
+    '#ffff99',
+    '#b15928'
+  ];
+
   /**
    * Creates new diagram model instance.
    * 
    * @param name Diagram name.
    */
-  constructor(public name:string) {}
+  constructor(public name:string) {
+    // add 3 starter topics for now
+    this.addTopic('Topic 1');
+    this.addTopic('Topic 2');
+    this.addTopic('Topic 3');    
+  }
+
+
+  /**
+   * Adds new topic to the diagram.
+   * 
+   * @param topicName Topic name.
+   */
+  addTopic(topicName:string) {
+    if (topicName) {
+      this.topics.push(
+        new Topic(topicName, this.getRandomColor(), this.getRandomColor() ) // stroke, fill
+      );
+    }
+  }
+
+
+  /**
+   * Gets a random color from configured color palette
+   * for diagrom topic stroke and fill color display.
+   */
+  private getRandomColor():string {
+    return this.colorPalette[(Math.random() * 12)];
+  }
+  
 }
