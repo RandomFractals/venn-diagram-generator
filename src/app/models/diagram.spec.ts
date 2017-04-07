46bc38c69c522d3205b267f3ec5d3770573864b7
Taras Novak <taras.novak@randomfractals.com>
2017-04-07 09:47:09 -0500

    added diagram.removeTopic() and test

--- src/app/models/diagram.spec.ts
+++ src/app/models/diagram.spec.ts
@@ -10,15 +10,21 @@ describe('Diagram', () => {
     expect(diagram.name).toEqual('Test Diagram');
   }));
 
-  it('new Diagram should have 3 topics to start with', async(() => {
+  it('new Diagram should have 3 Topics to start with', async(() => {
     const diagram:Diagram = new Diagram('Test Diagram');
     expect(diagram.topics.length).toEqual(3);
   }));
 
-  it('one can add new topic to a Diagram', async(() => {
+  it('one can add new Topic to the Diagram', async(() => {
     const diagram:Diagram = new Diagram('Test Diagram');
     const topic:Topic = diagram.addTopic('Test Topic');
     expect(diagram.topics.length).toEqual(4);
+  }));
+
+  it('one can remove a Topic from the Diagram', async(() => {
+    const diagram:Diagram = new Diagram('Test Diagram');
+    diagram.removeTopic(2); // remove last topic
+    expect(diagram.topics.length).toEqual(2);
   }));
 
 });
--- src/app/models/diagram.ts
+++ src/app/models/diagram.ts
@@ -42,17 +42,35 @@ export class Diagram {
    * Adds new topic to the diagram.
    * 
    * @param topicName Topic name.
+   * 
+   * @return Added topic instance, or null, if topic name is null.
    */
   addTopic(topicName:string) {
-    let topic:Topic;
+    let newTopic:Topic;
     if (topicName) {
       // create and add new topic
-      topic = new Topic(topicName, this.getRandomColor(), this.getRandomColor() ) // stroke, fill
-      this.topics.push(topic);
+      newTopic = new Topic(topicName, this.getRandomColor(), this.getRandomColor() ) // stroke, fill
+      this.topics.push(newTopic);
     }
-    return topic;
+    return newTopic;
   }
 
+
+  /**
+   * Removes a topic from the topics list.
+   * 
+   * @param topicIndex Topic index.
+   * 
+   * @return Removed topic instance or null.
+   */
+  removeTopic(topicIndex:number) {
+    let removedTopic:Topic;
+    if (topicIndex >= 0 && topicIndex < this.topics.length) {
+      removedTopic = this.topics.splice(topicIndex, 1)[0];
+    }
+    return removedTopic;
+  }
+  
 
   /**
    * Gets a random color from configured color palette
