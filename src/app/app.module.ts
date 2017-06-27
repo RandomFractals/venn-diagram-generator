c6a3a0195a218ac1eda347d81ae900cd4a86da3e
Taras Novak <taras.novak@randomfractals.com>
2017-06-27 08:11:19 -0500

    created app footer comp and moved footer html/css parts to it

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { AppFooterComponent } from './app-footer/app-footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppFooterComponent
   ],
   imports: [
     BrowserModule,
