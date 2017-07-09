16cf529dae45527c24de97c90e0302e19cb667fa
Taras Novak <taras.novak@gmail.com>
2017-07-08 20:34:28 -0600

    added cx,cy,rx,ry to topic view model for svg segments rendering

--- src/app/models/topic.ts
+++ src/app/models/topic.ts
@@ -3,6 +3,12 @@
  */
 export class Topic {
 
+  // topic svg ellipse position and radius view model properties
+  cx = 250;
+  cy = 250;
+  rx = 60;
+  ry = 60;
+
   /**
    * Creates new topic view model instance with name,
    * stroke and fill color properties
